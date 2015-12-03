1f8dcc95588076e7e35b7f93a3dd66c4a03b5d83
wojtus <wojtus@delta>
2015-12-03 16:31:28 +0100

    unit test for HasVisibleForTestingPredicate

--- src/main/java/de/tolina/sonar/plugins/vft/checks/HasVisibleForTestingPredicate.java
+++ src/main/java/de/tolina/sonar/plugins/vft/checks/HasVisibleForTestingPredicate.java
@@ -1,18 +1,33 @@
 package de.tolina.sonar.plugins.vft.checks;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Optional;
 import java.util.function.Predicate;
 
 import org.sonar.plugins.java.api.semantic.Symbol;
 import org.sonar.plugins.java.api.semantic.SymbolMetadata.AnnotationInstance;
 
+import com.google.common.annotations.VisibleForTesting;
+
 class HasVisibleForTestingPredicate implements Predicate<Symbol> {
-	private final Predicate<AnnotationInstance> isVisibleForTestingAnnotation = new IsVisibleForTesting();
+	private final Predicate<AnnotationInstance> isVisibleForTestingAnnotation;
+
+
+	HasVisibleForTestingPredicate() {
+		this(new IsVisibleForTesting());
+	}
+
+	@VisibleForTesting
+	HasVisibleForTestingPredicate(final Predicate<AnnotationInstance> isVisibleForTestingAnnotation) {
+		this.isVisibleForTestingAnnotation = isVisibleForTestingAnnotation;
+	}
+
 
 	@Override
 	public boolean test(Symbol symbol) {
-		final Collection<AnnotationInstance> annotations = symbol.metadata().annotations();
+		final Optional<Collection<AnnotationInstance>> annotationsOptional = Optional.ofNullable(symbol).map(s -> s.metadata()).map(m -> m.annotations());
+		final Collection<AnnotationInstance> annotations = annotationsOptional.orElse(Collections.emptyList());
 		final Optional<AnnotationInstance> annotationOptional = annotations.stream().filter(isVisibleForTestingAnnotation).findAny();
 		boolean hasVisibleForTesting = annotationOptional.isPresent();
 		return hasVisibleForTesting;
---
+++ src/test/java/de/tolina/sonar/plugins/vft/checks/HasVisibleForTestingPredicateTest.java
@@ -0,0 +1,84 @@
+package de.tolina.sonar.plugins.vft.checks;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.sonar.plugins.java.api.semantic.Symbol;
+import org.sonar.plugins.java.api.semantic.SymbolMetadata;
+import org.sonar.plugins.java.api.semantic.SymbolMetadata.AnnotationInstance;
+
+@SuppressWarnings("javadoc")
+@RunWith(MockitoJUnitRunner.class)
+public class HasVisibleForTestingPredicateTest {
+
+	private Predicate<Symbol> hasVisibleForTestingPredicate;
+
+	@Mock
+	private Predicate<AnnotationInstance> isVisibleForTestingAnnotation;
+	@Mock
+	private Symbol symbol;
+	@Mock
+	private SymbolMetadata metaData;
+
+	@Before
+	public void initTestObject() {
+		hasVisibleForTestingPredicate = new HasVisibleForTestingPredicate(isVisibleForTestingAnnotation);
+	}
+
+	@Before
+	public void initMock() {
+		when(symbol.metadata()).thenReturn(metaData);
+	}
+
+
+	@Test
+	public void testNoAnnotationsAtSymbol() throws Exception {
+		assertFalse(hasVisibleForTestingPredicate.test(symbol));
+
+	}
+
+	@Test
+	public void testOtherAnnotationsAtSymbol() throws Exception {
+		AnnotationInstance annotation1 = mock(AnnotationInstance.class);
+		AnnotationInstance annotation2 = mock(AnnotationInstance.class);
+		AnnotationInstance annotation3 = mock(AnnotationInstance.class);
+		List<AnnotationInstance> annotations = Arrays.asList(annotation1, annotation2, annotation3);
+		when(metaData.annotations()).thenReturn(annotations);
+
+		when(Boolean.valueOf(isVisibleForTestingAnnotation.test(annotation1))).thenReturn(Boolean.FALSE);
+		when(Boolean.valueOf(isVisibleForTestingAnnotation.test(annotation2))).thenReturn(Boolean.FALSE);
+		when(Boolean.valueOf(isVisibleForTestingAnnotation.test(annotation3))).thenReturn(Boolean.FALSE);
+		assertFalse(hasVisibleForTestingPredicate.test(symbol));
+
+	}
+
+	@Test
+	public void testVisibleForTestingAtSymbol() throws Exception {
+		AnnotationInstance annotation1 = mock(AnnotationInstance.class);
+		AnnotationInstance annotation2 = mock(AnnotationInstance.class);
+		AnnotationInstance annotation3 = mock(AnnotationInstance.class);
+		List<AnnotationInstance> annotations = Arrays.asList(annotation1, annotation2, annotation3);
+		when(metaData.annotations()).thenReturn(annotations);
+
+		when(Boolean.valueOf(isVisibleForTestingAnnotation.test(annotation1))).thenReturn(Boolean.FALSE);
+		when(Boolean.valueOf(isVisibleForTestingAnnotation.test(annotation2))).thenReturn(Boolean.TRUE);
+		when(Boolean.valueOf(isVisibleForTestingAnnotation.test(annotation3))).thenReturn(Boolean.FALSE);
+		assertTrue(hasVisibleForTestingPredicate.test(symbol));
+
+	}
+
+
+
+
+}
