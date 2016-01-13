3219d126bdbe33144080a17ac55634f6df863e54
Frank Jakop <frank.jakop@arxes-tolina.de>
2016-01-13 16:32:08 +0100

    increased readability of rule descriptions

--- src/test/java/de/tolina/sonar/plugins/vft/checks/MisuseOfVisibleForTestingCheckerTestClass.java
+++ src/test/java/de/tolina/sonar/plugins/vft/checks/MisuseOfVisibleForTestingCheckerTestClass.java
@@ -9,13 +9,13 @@ class MisuseOfVisibleForTestingCheckerTestClass {
 	//	/** commons-logging Logger für diese Klasse. Per Default auskommentiert */
 	//	private final transient Log log = LogFactory.getLog(this.getClass());
 
-	@VisibleForTesting // Noncompliant {{You must use @VisibleForTesting annotation only at package-private methods or package-private fields.}}
+	@VisibleForTesting // Noncompliant {{You can use <code>@VisibleForTesting</code> annotation only at package-private methods or package-private fields.}}
 	private Object privateObject = new Object();
 	@VisibleForTesting
 	Object defaultObject = new Object();
-	@VisibleForTesting // Noncompliant {{You must use @VisibleForTesting annotation only at package-private methods or package-private fields.}} 
+	@VisibleForTesting // Noncompliant {{You can use <code>@VisibleForTesting</code> annotation only at package-private methods or package-private fields.}} 
 	protected Object protectedObject = new Object();
-	@VisibleForTesting // Noncompliant {{You must use @VisibleForTesting annotation only at package-private methods or package-private fields.}} 
+	@VisibleForTesting // Noncompliant {{You can use <code>@VisibleForTesting</code> annotation only at package-private methods or package-private fields.}} 
 	public Object publicObject = new Object();
 
 	@VisibleForTesting
@@ -24,20 +24,20 @@ class MisuseOfVisibleForTestingCheckerTestClass {
 	}
 
 	@SuppressWarnings("unused")
-	@VisibleForTesting // Noncompliant {{You must use @VisibleForTesting annotation only at package-private methods or package-private fields.}}
+	@VisibleForTesting // Noncompliant {{You can use <code>@VisibleForTesting</code> annotation only at package-private methods or package-private fields.}}
 	private MisuseOfVisibleForTestingCheckerTestClass(Object obj) {
 		// NOOP
 	}
 
 	@SuppressWarnings("unused")
-	@VisibleForTesting // Noncompliant {{You must use @VisibleForTesting annotation only at package-private methods or package-private fields.}}
+	@VisibleForTesting // Noncompliant {{You can use <code>@VisibleForTesting</code> annotation only at package-private methods or package-private fields.}}
 	public MisuseOfVisibleForTestingCheckerTestClass(Object obj1, Object obj2) {
 		// NOOP
 	}
 
 
 
-	@VisibleForTesting // Noncompliant {{You must use @VisibleForTesting annotation only at package-private methods or package-private fields.}}
+	@VisibleForTesting // Noncompliant {{You can use <code>@VisibleForTesting</code> annotation only at package-private methods or package-private fields.}}
 	private void privateMethod() {
 		// NOOP
 	}
@@ -47,24 +47,24 @@ class MisuseOfVisibleForTestingCheckerTestClass {
 		// NOOP
 	}
 
-	@VisibleForTesting // Noncompliant {{You must use @VisibleForTesting annotation only at package-private methods or package-private fields.}}
+	@VisibleForTesting // Noncompliant {{You can use <code>@VisibleForTesting</code> annotation only at package-private methods or package-private fields.}}
 	protected void protectedMethod() {
 		// NOOP
 	}
 
 
-	@VisibleForTesting // Noncompliant {{You must use @VisibleForTesting annotation only at package-private methods or package-private fields.}}
+	@VisibleForTesting // Noncompliant {{You can use <code>@VisibleForTesting</code> annotation only at package-private methods or package-private fields.}}
 	public void publicMethod() {
 		// NOOP
 	}
 
-	@VisibleForTesting // Noncompliant {{You must use @VisibleForTesting annotation only at package-private methods or package-private fields.}}
+	@VisibleForTesting // Noncompliant {{You can use <code>@VisibleForTesting</code> annotation only at package-private methods or package-private fields.}}
 	private static class PrivateClass {
 		// NOOP
 	}
 
 
-	@VisibleForTesting // Noncompliant {{You must use @VisibleForTesting annotation only at package-private methods or package-private fields.}}
+	@VisibleForTesting // Noncompliant {{You can use <code>@VisibleForTesting</code> annotation only at package-private methods or package-private fields.}}
 	public static class PublicClass {
 		// NOOP
 	}
--- src/test/java/de/tolina/sonar/plugins/vft/checks/UnexpectedAccessCheckTestClassCaller.java
+++ src/test/java/de/tolina/sonar/plugins/vft/checks/UnexpectedAccessCheckTestClassCaller.java
@@ -10,36 +10,36 @@ class UnexpectedAccessCheckTestClassCaller {
 	//	private final transient Log log = LogFactory.getLog(this.getClass());
 
 
-	private static final UnexpectedAccessCheckTestClassCallee staticCallee = new UnexpectedAccessCheckTestClassCallee(); // Noncompliant {{You must not access package-private method or field which is annotated by @VisibleForTesting.}}
-	static final Object staticObject = staticCallee.visibleForTesting; // Noncompliant {{You must not access package-private method or field which is annotated by @VisibleForTesting.}}
-	static final Object staticObjectFromStatic = UnexpectedAccessCheckTestClassCallee.staticVisibleForTesting; // Noncompliant {{You must not access package-private method or field which is annotated by @VisibleForTesting.}}
+	private static final UnexpectedAccessCheckTestClassCallee staticCallee = new UnexpectedAccessCheckTestClassCallee(); // Noncompliant {{You must not access package-private method or field which is annotated by <code>@VisibleForTesting</code>.}}
+	static final Object staticObject = staticCallee.visibleForTesting; // Noncompliant {{You must not access package-private method or field which is annotated by <code>@VisibleForTesting</code>.}}
+	static final Object staticObjectFromStatic = UnexpectedAccessCheckTestClassCallee.staticVisibleForTesting; // Noncompliant {{You must not access package-private method or field which is annotated by <code>@VisibleForTesting</code>.}}
 
 	static {
-		staticCallee.methodeCallee(); // Noncompliant {{You must not access package-private method or field which is annotated by @VisibleForTesting.}}
+		staticCallee.methodeCallee(); // Noncompliant {{You must not access package-private method or field which is annotated by <code>@VisibleForTesting</code>.}}
 	}
 
-	private final UnexpectedAccessCheckTestClassCallee callee = new UnexpectedAccessCheckTestClassCallee(new Object());// Noncompliant {{You must not access package-private method or field which is annotated by @VisibleForTesting.}}
+	private final UnexpectedAccessCheckTestClassCallee callee = new UnexpectedAccessCheckTestClassCallee(new Object());// Noncompliant {{You must not access package-private method or field which is annotated by <code>@VisibleForTesting</code>.}}
 
 	void defaultMethodeCaller() {
-		callee.methodeCallee(); // Noncompliant {{You must not access package-private method or field which is annotated by @VisibleForTesting.}}
+		callee.methodeCallee(); // Noncompliant {{You must not access package-private method or field which is annotated by <code>@VisibleForTesting</code>.}}
 	}
 
 	@SuppressWarnings("unused")
 	private void privateMethodeCaller() {
-		callee.methodeCallee(); // Noncompliant {{You must not access package-private method or field which is annotated by @VisibleForTesting.}}
+		callee.methodeCallee(); // Noncompliant {{You must not access package-private method or field which is annotated by <code>@VisibleForTesting</code>.}}
 	}
 
 	public void publicMethodeCaller() {
-		callee.methodeCallee(); // Noncompliant {{You must not access package-private method or field which is annotated by @VisibleForTesting.}}
+		callee.methodeCallee(); // Noncompliant {{You must not access package-private method or field which is annotated by <code>@VisibleForTesting</code>.}}
 	}
 
 	protected void protectedMethodeCaller() {
-		callee.methodeCallee(); // Noncompliant {{You must not access package-private method or field which is annotated by @VisibleForTesting.}}
+		callee.methodeCallee(); // Noncompliant {{You must not access package-private method or field which is annotated by <code>@VisibleForTesting</code>.}}
 	}
 
 
 	void defaultAccesMember() {
 		Objects.requireNonNull(callee.visibleForAll);
-		Objects.requireNonNull(callee.visibleForTesting); // Noncompliant {{You must not access package-private method or field which is annotated by @VisibleForTesting.}}		
+		Objects.requireNonNull(callee.visibleForTesting); // Noncompliant {{You must not access package-private method or field which is annotated by <code>@VisibleForTesting</code>.}}		
 	}
 }
